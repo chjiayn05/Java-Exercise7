6dc3fd0881c9ee60c94a5d37702485f0b220e443
Xu0903 <rich040903@gmail.com>
2026-04-22 14:18:06 +0800

    完成程式骨幹並新增徐瑞利資料

---
+++ FirstClass4Team.java
@@ -0,0 +1,9 @@
+public class FirstClass4Team {
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01357125\"\n姓名：\"徐瑞利\"");
+        //commit meg: feat:完成程式骨幹並新增徐瑞利資料
+        //commit meg: feat:新增陳佳昀資料
+        //commit meg: feat:新增林良承資料
+    }
+}
