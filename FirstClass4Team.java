353b555229bcd937d50aab2b3b77675e3255d556
Lin Randy <liang@LindeMacBook-Pro.local>
2026-04-22 15:22:33 +0800

    feat:新增林良承資料

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,6 +3,7 @@ public class FirstClass4Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01357125\"\n姓名：\"徐瑞利\"");
         System.out.println("\"學號：\"01357122\"\n姓名：\"陳佳昀\"");
+        System.out.println("\"學號：\"01357132\"\n姓名：\"林良承\"");
         //commit meg: feat:完成程式骨幹並新增徐瑞利資料
         //commit meg: feat:新增陳佳昀資料
         //commit meg: feat:新增林良承資料
