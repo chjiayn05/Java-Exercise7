f8c2cba5b45f92752c602acfd20167e4ef92f12b
unknown <wtps5955linda@gmail.com>
2026-04-22 15:21:51 +0800

    feat:新增陳佳昀資料

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,6 +2,7 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01357125\"\n姓名：\"徐瑞利\"");
+        System.out.println("\"學號：\"01357122\"\n姓名：\"陳佳昀\"");
         //commit meg: feat:完成程式骨幹並新增徐瑞利資料
         //commit meg: feat:新增陳佳昀資料
         //commit meg: feat:新增林良承資料
